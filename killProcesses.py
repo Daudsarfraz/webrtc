e8cf58ecf00901cbd2994177362eea6ca24e5af4
Dawood Sarfraz <dawoodsarfraz.cs@gmail.com>
2025-01-29 14:55:59 +0500

    removed user permission

--- killProcesses.py
+++ killProcesses.py
@@ -3,7 +3,8 @@ import subprocess
 port = 8080  # Port i am using 8080 for the server running 
 
 # Find processes using the specified port
-command = f"sudo lsof -t -i:{port}"
+# command = f"sudo lsof -t -i:{port}"
+command = f"lsof -t -i:{port}"
 
 try:
     # Get the list of PIDs that are using the given port
@@ -13,7 +14,9 @@ try:
     if pid_list:
         # If there are processes, kill them
         for pid in pid_list:
+            # subprocess.run(f"sudo kill -9 {pid}", shell=True)
             subprocess.run(f"sudo kill -9 {pid}", shell=True)
+
         print(f"Killed processes running on port {port}.")
     else:
         # If no processes are found, print a message
